1f6758703603091975326abc769db2f79fcc9d3f
trung <tranthanhtrung1983@yahoo.com>
2026-03-16 17:27:04 +0100

    Setup Project

---
+++ tests/example.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+  
+  await expect(page.locator('h1')).toContainText('Login');
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
